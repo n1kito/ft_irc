a126b09576b83b526aa0e7ff78fdd375be7a40ae
Nikito <mjallada@student.42.fr>
2023-05-17 09:41:27 +0200

    replaces occurences of getModeParameter() with getKey() and getClientClimit(), that were just updated to work with the new setup

--- src/Commands/Channels/Join.cpp
+++ src/Commands/Channels/Join.cpp
@@ -72,7 +72,7 @@ std::string		Join::action(Client &client)
 		{
 			// TODO: MODE
 			// check if channel has set limit for nb users 
-			if (it->second.modeIs("client-limit") && it->second.getClientMap().size() >= static_cast< size_t >(std::atoi(it->second.getModeParameter("client-limit").c_str())))
+			if (it->second.modeIs("client-limit") && it->second.getClientMap().size() >= it->second.getClientLimit())
 				return (ERR_CHANNELISFULL(client.getServerName(), client.getNickname(), it->second.getName()));
 			if (it->second.modeIs("invite-only") && !it->second.isInvited(client.getNickname()))
 				return (ERR_INVITEONLYCHAN(client.getServerName(), client.getNickname(), it->second.getName()));
@@ -80,7 +80,7 @@ std::string		Join::action(Client &client)
 			// if (!it->second.getKey().empty())
 			{
 				// if key is incorrect, cannot join channel and send error
-                if (_keyList.empty() || (i < _keyList.size() && it->second.getModeParameter("key") != _keyList[i]))
+                if (_keyList.empty() || (i < _keyList.size() && it->second.getKey() != _keyList[i]))
 					return (ERR_BADCHANNELKEY(client.getServerName(), client.getNickname(), it->second.getName()));
 			}
 			it->second.addConnectedClient(client);
